6cc3852dfa84fd67bd94da863c84077775838302
reinsp5 <sk.reinsp5@gmail.com>
2021-10-20 20:34:44 +0900

    no message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,12 +30,28 @@ export default defineComponent({
     }
   },
   methods: {
-    analyze(){
-      this.kuromoji.build((err, tokenizer) => {
-        const path = tokenizer.tokenize(this.inputText);
-        this.output = path.filter(function(value){
-          return value.pos != "助詞" && value.pos != "記号"; // 記号と助詞は不要
+    async analyze(){
+      await this.kuromoji.build((err, tokenizer) => {
+        const path = tokenizer.tokenize(this.inputText); // 形態素解析実行
+        
+        // 解析結果から助詞と記号を排除する
+        const result = path.filter(function(value){
+          return value.pos != "助詞" && value.pos != "記号" && value.pos != "助動詞"; // 記号と助詞は不要
         });
+
+        // 不要な要素を削除した結果から、出現頻度リストを作成する
+        const list = [] as any;
+        result.forEach((element) => {
+          // リストに値が存在する
+          const record = list.find((v:any) => v.word_id === element.word_id);
+          if(record){
+            record.count += 1;
+          }else{
+            list.push({word_id: element.word_id, surface_form: element.surface_form, pos: element.pos, pos_detail_1: element.pos_detail_1, count: 1});
+          }
+        });
+        console.table(list);
+        this.output = result
       });
     }
   }
